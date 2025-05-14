9e3dacaac8be6fc93ace1bc5def348d1d8d65d34
Jalen-Moore <jrmoore@my.loyno.edu>
2025-05-14 12:32:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 import App from './App';
@@ -15,9 +15,11 @@ import './css/card.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename = '/portfolio'>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
